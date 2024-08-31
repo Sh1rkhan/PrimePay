8cfc1a626130344117cd22efdf64054fb89b5b9d
shamilvasiyev <vasiyevsamil@gmail.com>
2024-08-31 17:54:21 +0400

    test codes added

--- src/common/Icons/index.jsx
+++ src/common/Icons/index.jsx
@@ -1,8 +1,6 @@
 import IcoMoon from "react-icomoon";
 import iconSet from "./selection.json";
 
-const Icon = (props) => (
-  <IcoMoon iconSet={iconSet} {...props} />
-);
+const Icon = (props) => <IcoMoon iconSet={iconSet} {...props} />;
 
 export default Icon;
