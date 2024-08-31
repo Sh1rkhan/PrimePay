f1214342dc6c524a5fa7175c900579badefc9cd7
shamilvasiyev <vasiyevsamil@gmail.com>
2024-08-31 19:59:18 +0400

    setup finished

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,8 @@
 import Home from "./Home";
 import Licenses from "./Licenses";
 import Blog from "./Blog";
+import Protected from "./Protected";
+import BlogSingle from "./BlogSingle";
+import NotFound from "./404";
 
-export { Home, Licenses, Blog };
+export { Home, Licenses, Blog, Protected, BlogSingle, NotFound };
