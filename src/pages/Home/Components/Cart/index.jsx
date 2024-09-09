6da1e3f2b41f2461269dfd195ca05fae712b2fd6
Sh1rkhan <huseynov.shirkhan@gmail.com>
2024-09-09 19:41:53 +0400

    add home page

---
+++ src/pages/Home/Components/Cart/index.jsx
@@ -0,0 +1,21 @@
+
+
+const Cart = ({ imageSrc, title, description }) => {
+
+  if (!imageSrc || !title || !description) {
+
+    return null; 
+  }
+
+  return (
+    <div className="w-[400px] border border-[#F8F8F8] rounded-[30px] shadow-[15px_-15px_2px_0px_rgba(0,_0,_0,_0.1)] m-[20px] bg-[#F8F8F8]">
+      <img src={imageSrc} alt={title} className="pt-[20px] pl-[20px]" />
+      <div className="p-[40px]">
+        <h1 className="mb-[10px] text-[35px] text-[#3D3269] font-[500]">{title}</h1>
+        <p className="m-0 text-[20px] text-[#4E4E4E] font-[400]">{description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
---
+++ src/pages/Home/Components/RatestList/index.jsx
@@ -0,0 +1,29 @@
+
+
+const RatestList = ({ imgSrc, name, role, rating, text }) => {
+    return (
+        <>
+         <div className="bg-white p-[40px_70px] w-[70%] rounded-[20px] shadow-[11px_-10px_10px_rgba(0,_0,_0,_0.1)] mb-[30px]" >
+            <div className="mr-[20px] ">
+                <img src={imgSrc} alt="foto"  className="rounded-full w-[80px] h-[80px]" />
+            </div>
+            <div className="flex-1">
+                <div className="flex justify-between items-center mb-2 cursor-pointer">
+                    <div className="text-left">
+                        <h3 className="text-lg font-bold m-0 text-[#3D3269] ">{name}</h3>
+                        <a href="#" className="text-[#625FFB] cursor-pointer hover:underline">{role}</a>
+                    </div>
+                    <div className="text-4xl text-[#FFB545] mb-5">
+                        {'★'.repeat(rating)}
+                    </div>
+                </div>
+                <hr className="border-t border-[#e0e0e0] my-2.5" />
+                <p className="text-base text-[#555] text-left">{text}</p>
+            </div>
+        </div>
+        </>
+
+    );
+};
+  
+  export default RatestList;
